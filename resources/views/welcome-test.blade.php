711fd1fdac62318f17fcce186202af6eb167d8d4
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-08-09 20:30:26 +0700

    test ci

--- resources/views/welcome-test.blade.php
+++ resources/views/welcome-test.blade.php
@@ -12,7 +12,7 @@
 
 <body class="antialiased flex items-center justify-center min-h-screen bg-gray-100">
     <div class="text-center">
-        <h1 class="text-4xl font-bold mb-6">Selamat Datang</h1>
+        <h1 class="text-4xl font-bold mb-6">Selamat Datang di Web Inventory</h1>
 
         <div class="space-x-4">
             <a href="{{ route('login') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
